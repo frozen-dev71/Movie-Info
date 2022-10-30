98d041090561f052a50ea03f484357f7ec1a28ed
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-10-31 01:26:45 +0300

    Created movie Group and Image Size Const
    We will use this const to reduce our code size and and it will help us keep our code clean

---
+++ src/app/constants/movie.groups.ts
@@ -0,0 +1,9 @@
+const movieGroupPopular = 'popular';
+const movieGroupTopRated = 'top_rated';
+const movieGroupUpcoming = 'upcoming';
+
+export const MOVIE_GROUPS = {
+	popular: movieGroupPopular,
+	topRated: movieGroupTopRated,
+	upcoming: movieGroupUpcoming
+}
