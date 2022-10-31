d3d3db9a2fc82cac53785d40cb1ba3be803bec26
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-10-31 14:43:16 +0300

    Created links to movie details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
@@ -7,6 +8,10 @@ const routes: Routes = [
     path: '',
     component: HomeComponent
   },
+  {
+    path: 'movie/:id',
+    component: MovieDetailComponent
+  }
 ];
 
 @NgModule({
